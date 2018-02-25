7048d26e8e35566ba05f17f60381bc5ae51e48b3
David Florness <edwargix@gmail.com>
2018-02-25 08:41:59 -0700

    Store color to db

--- color-lambda/lambda_handler.py
+++ color-lambda/lambda_handler.py
@@ -5,13 +5,16 @@ import io
 from math import sqrt
 import json
 
+import boto3
+
 
 threshold = 120
 
 
 def lambda_handler(event, context):
     j = json.loads(event['body'])
-    f = io.BytesIO(base64.b64decode(j['image']))
+    b64 = j['image']
+    f = io.BytesIO(base64.b64decode(b64))
     im = Image.open(f)
 
     sums = [0, 0, 0]
@@ -27,7 +30,23 @@ def lambda_handler(event, context):
                 sums[2] += pixel[2] ** 2
                 n = n + 1
 
+    item_id = int(j['id'])
+
     sums = map(lambda x: sqrt(x / n), sums)
+
+    dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+    table = dynamodb.Table('InventoryTracking')
+
+    table.update_item(
+        Key={
+            'id': item_id
+        },
+        UpdateExpression="set info.color = :c, info.image = :i",
+        ExpressionAttributeValues={
+            ':c': ','.join(map(str, sums)),
+            ':i': b64,
+        },
+    )
 
     response = {
         'statusCode': 200,
