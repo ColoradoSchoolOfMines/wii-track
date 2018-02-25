9eba3ac69944a3e7d5d8fc8448e5174d8bb78c3e
Sumner Evans <sumner.evans98@gmail.com>
2018-02-24 20:24:46 -0700

    dynamodb tutorial

---
+++ dynamodb/tutorial/MoviesItemOps03.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python3
+import boto3
+import json
+import decimal
+
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
+
+table = dynamodb.Table('Movies')
+
+title = "Sassy Pants"
+year = 2012
+
+response = table.update_item(
+    Key={
+        'year': year,
+        'title': title
+    },
+    UpdateExpression="set info.rating = :r, info.plot=:p, info.actors=:a",
+    ExpressionAttributeValues={
+        ':r': decimal.Decimal(5.5),
+        ':p': "Everything happens all at once.",
+        ':a': ["Larry", "Moe", "Curly", "Sumner"]
+    },
+    ReturnValues="UPDATED_NEW"
+)
+
+print("UpdateItem succeeded:")
+print(json.dumps(response, indent=4, cls=DecimalEncoder))
