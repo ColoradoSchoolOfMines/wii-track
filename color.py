57e27ee8e5c2b63b9eb2b4b8c9f5224e9ab6a9d3
David Florness <edwargix@gmail.com>
2018-02-25 01:44:55 -0700

    Function to calculate color of white-free image

---
+++ color.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from PIL import Image
+import base64
+import io
+from math import sqrt
+
+
+threshold = 120
+
+
+def read_image(b):
+    f = io.BytesIO(b)
+    im = Image.open(f)
+
+    sums = [0, 0, 0]
+    n = 0
+    for x in range(im.size[0]):
+        for y in range(im.size[1]):
+            pixel = im.getpixel((x, y))
+            if pixel[0] < threshold or \
+               pixel[1] < threshold or \
+               pixel[2] < threshold:
+                sums[0] += pixel[0] ** 2
+                sums[1] += pixel[1] ** 2
+                sums[2] += pixel[2] ** 2
+                n = n + 1
+
+    sums = map(lambda x: sqrt(x / n), sums)
+
+    print(list(sums))
